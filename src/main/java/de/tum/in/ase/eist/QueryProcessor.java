db32bb7d3ea9d8a7e464e34df7b7fc34e3b3f7ae
dani_ <ge26wof@mytum.de>
2022-07-13 15:12:42 +0200

    Sum

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -4,18 +4,30 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class QueryProcessor {
-
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "Daniela";
+            return "Daniela";
+        } else if (query.contains("plus")) {
+            // We get an array of words by splitting the whole string
+            String[] words = query.split("");
+            int sum = 0;
+            // Iterate on each word of the array to detect which contains a number
+            for (String word : words) {
+                try {
+                    int number = Integer.parseInt(word);
+                    sum += number;
+                } catch (Exception e) {
+                    //
+                }
+            }
+            return String.valueOf(sum);
+        } else if (query.contains("largest")) {
         }
-        // Add comment
-            return "";
+        return "";
         }
     }
-
